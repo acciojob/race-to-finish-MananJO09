2abf079d46432c306eff64e1c5887f487b01ce22
MananJO09 <87749897+MananJO09@users.noreply.github.com>
2023-06-01 22:48:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
-window.promises = [];
+function getRandomTime(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-// Do not change the code above this
-// add your promises to the array `promises`
+function createPromise() {
+  return new Promise((resolve) => {
+    const randomTime = getRandomTime(1000, 5000);
+    setTimeout(() => {
+      resolve(randomTime);
+    }, randomTime);
+  });
+}
+
+const promises = [];
+for (let i = 0; i < 5; i++) {
+  promises.push(createPromise());
+}
+
+Promise.any(promises)
+  .then((result) => {
+    const outputDiv = document.getElementById('output');
+    outputDiv.innerText = `The first promise resolved with ${result} milliseconds.`;
+  })
+  .catch((error) => {
+    console.error(error);
+  });
