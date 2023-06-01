43afcd71368eafc86d81a81beb68e3c2ef5a66c6
MananJO09 <87749897+MananJO09@users.noreply.github.com>
2023-06-01 23:25:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-function getRandomTime(min, max) {
+cy.window().then(win => {
+  // ...
+	function getRandomTime(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function createPromise() {
+  const promises = win.promises.map(
+	function createPromise() {
   return new Promise((resolve) => {
     const randomTime = getRandomTime(1000, 5000);
     setTimeout(() => {
       resolve(randomTime);
     }, randomTime);
   });
-}
+});
 
+// Move the Promise.any() call here, outside the cy.window().then() block
 Promise.any(promises)
   .then((result) => {
     const outputDiv = document.getElementById("output");
@@ -19,3 +21,6 @@ Promise.any(promises)
   .catch((error) => {
     console.error("An error occurred:", error);
   });
+}
+
+
